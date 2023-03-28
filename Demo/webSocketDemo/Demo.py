23c50d9ad7b77ee91b9ff010eb9b68e53957ace3
hxy <1305149167@qq.com>
2023-03-29 00:03:21 +0800

    添加qq信息

--- Demo/webSocketDemo/Demo.py
+++ Demo/webSocketDemo/Demo.py
@@ -2,7 +2,7 @@
 Author: hxy 1305149167@qq.com
 Date: 2022-07-31 06:48:29
 LastEditors: hxy 1305149167@qq.com
-LastEditTime: 2023-03-22 01:32:37
+LastEditTime: 2023-03-24 01:02:27
 FilePath: /webSocketDemo/Demo.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -10,5 +10,10 @@ import random as rd
 
 x = rd.randint(0, 10)
 print(x)
-with open("example.txt", "x") as file:
-    file.write("Hello, world!")
+# with open("example.txt", "r") as file:
+#     content = file.read()
+#     print(content)
+f = open("example.txt", "r")
+s = f.readline()
+print(s)
+f.close()
